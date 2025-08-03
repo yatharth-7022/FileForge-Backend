bb08cf6fe75dd6445fcc7a8db88b0fce0479e177
yatharth7022 <yatharth7022@gmail.com>
2025-08-03 14:35:22 -0400

    adding cors config

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 const express = require("express");
 require("dotenv").config();
+const cors = require("cors");
 const authRoutes = require("./routes/auth");
 const uploadRoutes = require("./routes/upload");
 const app = express();
+
+const allowedOrigins = process.env.FRONTEND_URL
+  ? process.env.FRONTEND_URL.split(",")
+  : ["http://localhost:3000"];
+// CORS Configuration
+app.use(
+  cors({
+    origin: allowedOrigins, // Allow your frontend URL
+    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allowed HTTP methods
+    allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+  })
+);
 
 const PORT = 5000;
 app.use(express.json());
